08db113ffb3c6ea794f78cb8c5c7da4be0dd250a
Matin De Wet <matindewet116@gmail.com>
2025-10-05 16:52:17 +0200

    addition of migration for search field for category and category group

---
+++ Src/Services/WebApi/WebApi.Infrastructure/Data/Migrations/20251005142934_UpdateCategoryAndCategoryGroup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace WebApi.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCategoryAndCategoryGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<NpgsqlTsVector>(
+                name: "SearchVector",
+                schema: "public",
+                table: "CategoryGroup",
+                type: "tsvector",
+                nullable: false)
+                .Annotation("Npgsql:TsVectorConfig", "english")
+                .Annotation("Npgsql:TsVectorProperties", new[] { "Name", "Description" });
+
+            migrationBuilder.AddColumn<NpgsqlTsVector>(
+                name: "SearchVector",
+                schema: "public",
+                table: "Category",
+                type: "tsvector",
+                nullable: false)
+                .Annotation("Npgsql:TsVectorConfig", "english")
+                .Annotation("Npgsql:TsVectorProperties", new[] { "Name", "Description" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryGroup_SearchVector",
+                schema: "public",
+                table: "CategoryGroup",
+                column: "SearchVector")
+                .Annotation("Npgsql:IndexMethod", "GIN");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_SearchVector",
+                schema: "public",
+                table: "Category",
+                column: "SearchVector")
+                .Annotation("Npgsql:IndexMethod", "GIN");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CategoryGroup_SearchVector",
+                schema: "public",
+                table: "CategoryGroup");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Category_SearchVector",
+                schema: "public",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "SearchVector",
+                schema: "public",
+                table: "CategoryGroup");
+
+            migrationBuilder.DropColumn(
+                name: "SearchVector",
+                schema: "public",
+                table: "Category");
+        }
+    }
+}

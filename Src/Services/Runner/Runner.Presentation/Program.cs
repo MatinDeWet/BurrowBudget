07674b693354215f742e4e1af69e392e69f57549
Matin De Wet <matindewet116@gmail.com>
2025-10-11 15:27:10 +0200

    updated the runner for service registration

--- Src/Services/Runner/Runner.Presentation/Program.cs
+++ Src/Services/Runner/Runner.Presentation/Program.cs
@@ -1,6 +1,19 @@
+using Blob.Integration;
+using CQRS;
+using Repository.Core;
+using Runner.Application;
+using Runner.Infrastructure;
 using Runner.Presentation;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddCQRSSupport(typeof(IApplicationPointer));
+
+builder.Services.AddDatabase(builder.Configuration, builder.Environment.IsDevelopment() || builder.Environment.IsStaging());
+builder.Services.AddRepositories(typeof(IInfrastructurePointer));
+
+builder.Services.AddBlobSupport(builder.Configuration);
+
 builder.Services.AddHostedService<Worker>();
 
 IHost host = builder.Build();

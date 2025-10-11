b61cd99d3325684fcb264d1d085c6141bb800c5a
Matin De Wet <matindewet116@gmail.com>
2025-10-11 08:58:31 +0200

    setup function app with service registration

---
+++ Src/Services/Runner/Runner.Infrastructure/Data/Contexts/BudgetContext.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Core.Constants;
+using Microsoft.EntityFrameworkCore;
+
+namespace Runner.Infrastructure.Data.Contexts;
+public class BudgetContext : DbContext
+{
+    public BudgetContext() { }
+
+    public BudgetContext(DbContextOptions<BudgetContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(BudgetContext).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(SchemaConstants).Assembly);
+    }
+}
--- Src/Services/Runner/Runner.Presentation/Program.cs
+++ Src/Services/Runner/Runner.Presentation/Program.cs
@@ -1,7 +1,19 @@
+using Blob.Integration;
+using CQRS;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
+using Repository.Core;
+using Runner.Application;
+using Runner.Infrastructure;
 
 FunctionsApplicationBuilder builder = FunctionsApplication.CreateBuilder(args);
+
+builder.Services.AddCQRSSupport(typeof(IApplicationPointer));
+
+builder.Services.AddDatabase(builder.Configuration, builder.Environment.IsDevelopment() || builder.Environment.IsStaging());
+builder.Services.AddRepositories(typeof(IInfrastructurePointer));
+
+builder.Services.AddBlobSupport(builder.Configuration);
 
 builder.ConfigureFunctionsWebApplication();
 

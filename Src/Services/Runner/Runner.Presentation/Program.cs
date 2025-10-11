59ac0a446681e83885ab69ed55be31b62554b624
Matin De Wet <matindewet116@gmail.com>
2025-10-11 15:07:49 +0200

    removed azure function, and moved to .net worker, updated docker compose files to use rabbitmq, and minio

--- Src/Services/Runner/Runner.Presentation/Program.cs
+++ Src/Services/Runner/Runner.Presentation/Program.cs
@@ -1,25 +1,7 @@
-using Blob.Integration;
-using CQRS;
-using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.Hosting;
-using Repository.Core;
-using Runner.Application;
-using Runner.Infrastructure;
+using Runner.Presentation;
 
-FunctionsApplicationBuilder builder = FunctionsApplication.CreateBuilder(args);
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
 
-builder.Services.AddCQRSSupport(typeof(IApplicationPointer));
-
-builder.Services.AddDatabase(builder.Configuration, builder.Environment.IsDevelopment() || builder.Environment.IsStaging());
-builder.Services.AddRepositories(typeof(IInfrastructurePointer));
-
-builder.Services.AddBlobSupport(builder.Configuration);
-
-builder.ConfigureFunctionsWebApplication();
-
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
-
-builder.Build().Run();
+IHost host = builder.Build();
+host.Run();
